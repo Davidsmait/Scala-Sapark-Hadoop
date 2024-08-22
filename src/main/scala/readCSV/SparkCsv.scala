fa4ee6cc2eb8d541f2f189ec5a9b44c0dea59ba5
davidsan <david.sanluis@qrsof.com>
2024-08-22 11:53:33 -0600

    init: -  csv

---
+++ src/main/scala/readCSV/SparkCsv.scala
@@ -0,0 +1,21 @@
+package readCSV
+
+import org.apache.spark.sql.SparkSession
+
+object SparkCsv {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("spark-csv-test")
+      .master("local[*]")
+      .config("spark.driver.bindAddress","192.168.0.47")
+      .getOrCreate()
+
+    val df = spark.read
+      .option("header", value = true)
+      .csv("src/main/resources/AAPL.csv")
+
+    df.show()
+  }
+
+  }
